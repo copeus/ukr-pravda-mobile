d2fce7c6b37cdb9cef17af4645402f44945b34dd
unknown <Boss@.(none)>
2013-03-29 13:48:04 +0200

    Project core was full refactored Fixed bug with timeout on load Fixed a lot of different bugs Application prepared for additional rss parts

--- www/main.js
+++ www/main.js
@@ -1,13 +1,142 @@
 $(document).ready(function() {
 	
-	
-	var url_news = "http://pipes.yahoo.com/pipes/pipe.run?_id=4d625dfe6977e71acb45db4aa51726a6&_render=json&feedurl=http://www.pravda.com.ua/rss/view_news/";
-	var url_mainnews = "http://pipes.yahoo.com/pipes/pipe.run?_id=4d625dfe6977e71acb45db4aa51726a6&_render=json&feedurl=http://www.pravda.com.ua/rss/view_mainnews/";
-	var url_articles = "http://pipes.yahoo.com/pipes/pipe.run?_id=4d625dfe6977e71acb45db4aa51726a6&_render=json&feedurl=http://www.pravda.com.ua/rss/view_pubs/";
-	var url_blogs = "http://pipes.yahoo.com/pipes/pipe.run?_id=4d625dfe6977e71acb45db4aa51726a6&_render=json&feedurl=http://blogs.pravda.com.ua/rss/";
+	initInterface();
+	applyLocalSettings();
 	
 	
-	$.mobile.loading( 'show' );
+	function initInterface(){
+		$( "div[data-role='header']" ).html(		
+			$( "#header-tmpl" ).render()
+		).trigger( "create" );
+		
+		$("div[data-role=header].withbar").after(		
+			$( "#navbar-tmpl" ).render()
+		);
+		
+		$( "div[data-role='navbar']").navbar();
+		
+
+		$("div[data-role='navbar'] a").each(function(){
+			//console.log($(this).attr('href'), "#"+$(this).parents("div[data-role='page']").attr('id'))
+			if($(this).attr('href') == ("#"+$(this).parents("div[data-role='page']").attr('id')) )
+				$(this).addClass("ui-btn-active ui-state-persist")
+		})
+	};
+	
+	/*
+	 * Universal class, that return ask for RSS and render block  
+	 */	
+	function RSS_viewer (url_rss, selector_html_block, selector_item_for_render, selector_click_item, onRender = function() {} ) {
+    	
+    	this.url_rss = url_rss;
+    	this.selector_html_block = selector_html_block;
+    	this.selector_item_for_render = selector_item_for_render;
+    	this.selector_click_item = selector_click_item;
+    	this.onRender = onRender;
+    	this.data_items = [];
+    	
+    	    	
+    	this.renderHTML = function(){
+    		$( this.selector_html_block).html(
+				$(this.selector_item_for_render).render( this.data_items )
+			);
+			/*
+	 		* Bind item click  
+		 	*/
+	
+			$( this.selector_click_item ).bind('click',{data_items: this.data_items}, function(event){
+				anchor = $(this).is("a") ? $(this) : $(this).find("a");
+				curr_key = /#item\-(\d+)/g.exec(anchor.attr('href'))[1] ;
+				$.curr_item = event.data.data_items[curr_key]; 
+				$( "#textpage #content" ).html(		
+					$( "#news-full-item" ).render( $.curr_item )
+				);
+				$.mobile.changePage( $("#textpage"), { transition: "turn"} );	
+			});
+			
+			this.onRender();	
+			
+    	}
+
+    	this.getData = function() {
+    		$.mobile.loading( 'show' );	
+			$.ajax({
+				dataType: "json",
+				url: this.url_rss,
+				success: this.processData,
+				context: this
+			}); 
+		}
+		
+		this.processData = function(data) {
+			this.data_items = data.value.items[0].channel.item;
+			$.each(this.data_items, function(key, val) {
+				pubDate = new Date(val.pubDate);
+				val.pubTime = (pubDate.getHours() <=9 ?  '0'+pubDate.getHours(): pubDate.getHours() ) + ':'+   (pubDate.getMinutes() <=9 ?  '0'+pubDate.getMinutes(): pubDate.getMinutes() );
+				m = pubDate.getMonth() +1;
+				val.displayDate = (pubDate.getDay() <=9 ?  '0'+pubDate.getDay(): pubDate.getDay() ) + '.'+   (m <=9 ?  '0'+m: m )+'.'+pubDate.getFullYear(); 
+				val.pubTimeDate = (pubDate.getDay() <=9 ?  '0'+pubDate.getDay(): pubDate.getDay() ) + '.'+   (m <=9 ?  '0'+m: m )+ ' ' + val.pubTime
+			
+				var author_regex = /\(.*\)/ig;			
+				val.displayAuhtor = (typeof val.fulltext !="undefined") ? author_regex.exec(val.author) : 'Українська правда';
+				
+				val.description = (val.description== null) ? ((typeof val.fulltext !="undefined") ? val.fulltext.slice(0, 100) + '...': val.title) : val.description;
+			
+				val.fulltext = (typeof val.fulltext !="undefined") ? '<p>' + val.fulltext.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '</p><p>' + '$2')+ '</p>' : val.description;
+				val.fulltext = ( val.fulltext == null)? val.description : val.fulltext.replace("<p></p>", '','g') ;
+			});
+			
+			$.mobile.loading( 'hide' );
+			
+			this.renderHTML();
+		}
+		
+		this.getData();
+		
+	}
+
+	
+	var js2jsp_service = "http://pipes.yahoo.com/pipes/pipe.run?_id=4d625dfe6977e71acb45db4aa51726a6&_render=json&feedurl=";
+	
+	var items_containters  = [
+		new RSS_viewer(js2jsp_service+"http://www.pravda.com.ua/rss/view_news/","#newspage dl.news-title", "#news-title-items" , "#newspage dl.news-title dd" ),
+		new RSS_viewer(js2jsp_service+"http://www.pravda.com.ua/rss/view_mainnews/","#mainpage dl.news-title", "#news-title-items" , "#mainpage dl.news-title dd" ),
+		new RSS_viewer(js2jsp_service+"http://www.pravda.com.ua/rss/view_pubs/","#articlespage div.articles", "#article-title-items" , "div.article-title", onArticleRender ) ,
+		new RSS_viewer(js2jsp_service+"http://blogs.pravda.com.ua/rss/","#blogspage div.blogs", "#blog-title-items" , "div.blog-title", onBlogsRender) ,
+	];
+	
+	function onArticleRender(){
+		$('#articlespage div.articles div.article-title').slice(0,4).clone(true).appendTo('#mainpage div.articles');
+	}
+	
+	function onBlogsRender(){
+		$('#blogspage div.blogs div.blog-title').slice(0,2).clone(true).appendTo('#mainpage div.blogs');
+	}
+
+	/*
+	 * applyLocalSettings running on start and restore user settings from localDB
+	 */
+	function applyLocalSettings() {
+		var font_size = window.localStorage.getItem("font-size");
+		font_size = (typeof font_size  !="undefined") ? font_size : 13;
+		$("body").css('font-size',font_size+'px');
+		$( "#settingspage #font-size").val(font_size);
+	}
+    
+	/*
+	 * Settings page font slider
+	 */
+	$( "#settingspage").on('change','#font-size',function (event) {
+			$("#settingspage p:first").css('font-size',$(this).val()+'px' )
+	});
+		
+	/*
+	 * Save settings function
+	 */
+	$( "#settingspage .back-button").click(function () {
+			window.localStorage.setItem("font-size", $( "#settingspage #font-size").val());
+			applyLocalSettings();
+	});
 	
 	$("#refreshbtn").click(function() {
 		/*
@@ -18,197 +147,10 @@ $(document).ready(function() {
     	});
     	* 
 		 */
-		window.location.reload()
+		var url = $(this).attr('href');
+		$.mobile.changePage( url, { reloadPage: true, transition: "none"} );
+		//window.location.reload()
 	});
 	
-	
-	function applyLocalSettings() {
-		var font_size = window.localStorage.getItem("font-size");
-		font_size = (typeof font_size  !="undefined") ? font_size : 13;
-		$("body").css('font-size',font_size+'px');
-		$( "#settingspage #font-size").val(font_size);
-	}
-    
-	applyLocalSettings();
-	
-	/*
-	 * Get and process data for news 
-	 */
-	$.getJSON(url_news, function(data) {
-		
-		$.data_news = data.value.items[0].channel.item;
-		$.each($.data_news, function(key, val) {
-			pubDate = new Date(val.pubDate);
-			$.data_news[key].pubTime = (pubDate.getHours() <=9 ?  '0'+pubDate.getHours(): pubDate.getHours() ) + ':'+   (pubDate.getMinutes() <=9 ?  '0'+pubDate.getMinutes(): pubDate.getMinutes() );
-			val.fulltext = (typeof val.fulltext !="undefined") ? '<p>' + val.fulltext.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '</p><p>' + '$2')+ '</p>' : val.description;
-			val.fulltext = ( val.fulltext == null)? val.description : val.fulltext.replace("<p></p>", '','g') ;
-		});
-		
-		$( "#newspage dl.news-title" ).html(
-			$( "#news-title-items" ).render( $.data_news )
-		);
-		
-		/*
-	 	* News page news item click  
-		 */
-	
-		$( "#newspage dl.news-title dd" ).bind('click', function(){
-				anchor = $(this).is("a") ? $(this) : $(this).find("a");
-				$.curr_key = /#news\-(\d+)/g.exec(anchor.attr('href'))[1] ;
-				$( "#textpage #content" ).html(		
-					$( "#news-full-item" ).render( $.data_news[$.curr_key] )
-				);
-				$.mobile.changePage( $("#textpage"), { transition: "turn"} );	
-		});
-		
-		$.mobile.loading( 'hide' );
-	});
-
-	/*
-	 * Get and process data for main news 
-	 */
-	$.getJSON(url_mainnews, function(data) {
-		
-		$.data_mainnews = data.value.items[0].channel.item;
-		
-		$.each($.data_mainnews, function(key, val) {
-			pubDate = new Date(val.pubDate);
-			$.data_mainnews[key].pubTime = (pubDate.getHours() <=9 ?  '0'+pubDate.getHours(): pubDate.getHours() ) + ':'+   (pubDate.getMinutes() <=9 ?  '0'+pubDate.getMinutes(): pubDate.getMinutes() );
-			val.fulltext = (typeof val.fulltext !="undefined") ? '<p>' + val.fulltext.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '</p><p>' + '$2')+ '</p>' : val.description;
-			val.fulltext = ( val.fulltext == null)? val.description : val.fulltext.replace("<p></p>", '','g') ;
-		});
-		
-		$( "#mainpage dl.news-title" ).html(
-			$( "#news-title-items" ).render( $.data_mainnews )
-		);
-		
-		/*
-	 	* Main page news item click  
-		 */
-	
-		$( "#mainpage dl.news-title dd" ).bind('click', function(){
-				anchor = $(this).is("a") ? $(this) : $(this).find("a");
-				$.curr_key = /#news\-(\d+)/g.exec(anchor.attr('href'))[1] ;
-				$( "#textpage #content" ).html(		
-					$( "#news-full-item" ).render( $.data_mainnews[$.curr_key] )
-				);
-				$.mobile.changePage( $("#textpage"), { transition: "turn"} );	
-		});
-	
-	});
-
-
-	/*
-	 * Get and process data for articles 
-	 */	
-	$.getJSON(url_articles, function(data) {
-		
-		$.data_articles = data.value.items[0].channel.item;
-		
-		$.each($.data_articles, function(key, val) {
-			pubDate = new Date(val.pubDate);
-			m = pubDate.getMonth() +1;
-			$.data_articles[key].displayDate = (pubDate.getDay() <=9 ?  '0'+pubDate.getDay(): pubDate.getDay() ) + '.'+   (m <=9 ?  '0'+m: m )+'.'+pubDate.getFullYear(); 
-			
-			var author_regex = /\(.*\)/ig;			
-			$.data_articles[key].displayAuhtor = author_regex.exec(val.author);
-
-			val.fulltext = (typeof val.fulltext !="undefined") ? '<p>' + val.fulltext.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '</p><p>' + '$2')+ '</p>' : val.description;
-			val.fulltext = ( val.fulltext == null)? val.description : val.fulltext.replace("<p></p>", '','g') ;
-
-		});
-		
-		
-		$( "#mainpage div.articles" ).html(
-			$( "#article-title-items" ).render( $.data_articles.slice(0,4) )
-		);
-		
-		$( "#articlespage div.articles" ).html(
-			$( "#article-title-items" ).render( $.data_articles )
-		);
-		
-		/*
-	 	* Main page and aricles page article item click  
-	 	*/
-	
-		$( "div.article-title" ).bind('click',function(){
-				anchor = $(this).is("a") ? $(this) : $(this).find("a");
-				$.curr_key = /#article\-(\d+)/g.exec(anchor.attr('href'))[1] ;
-				$( "#textpage #content" ).html(		
-					$( "#article-full-item" ).render( $.data_articles[$.curr_key] )
-				);
-				$.mobile.changePage( $("#textpage"), { transition: "turn"} );	
-		});
-	
-	});
-	
-	/*
-	 * Get and process data for blogs 
-	 */	
-	$.getJSON(url_blogs, function(data) {
-		
-		$.data_blogs = data.value.items[0].channel.item;
-		
-		$.each($.data_blogs, function(key, val) {
-			pubDate = new Date(val.pubDate);
-			m = pubDate.getMonth() +1;
-			$.data_blogs[key].displayDate = (pubDate.getDay() <=9 ?  '0'+pubDate.getDay(): pubDate.getDay() ) + '.'+   (m <=9 ?  '0'+m: m )+'.'+pubDate.getFullYear(); 
-			
-			var author_regex = /\(.*\)/ig;			
-			$.data_blogs[key].displayAuhtor = author_regex.exec(val.author);
-
-			val.description = (val.description== null) ? val.fulltext.slice(0, 100) + '...' : val.description;
-			
-			val.fulltext = (typeof val.fulltext !="undefined") ? '<p>' + val.fulltext.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '</p><p>' + '$2')+ '</p>' : val.description;
-			val.fulltext = ( val.fulltext == null)? val.description : val.fulltext.replace("<p></p>", '','g') ;
-			
-			
-
-		});
-		
-		$( "#blogspage div.blogs" ).html(
-			$( "#blog-title-items" ).render( $.data_blogs )
-		);
-		
-			
-		/*
-		 * Blogs page item click  
-		 */
-	
-		$( "div.blog-title" ).bind('click', function(){
-				anchor = $(this).is("a") ? $(this) : $(this).find("a");
-				$.curr_key = /#blog\-(\d+)/g.exec(anchor.attr('href'))[1] ;
-				$( "#textpage #content" ).html(		
-					$( "#blog-full-item" ).render( $.data_blogs[$.curr_key] )
-				);
-				$.mobile.changePage( $("#textpage"), { transition: "turn"} );	
-		});
-		
-		
-	});	
-
-	
-
-
-	
-
-	
-	/*
-	 * Settings page font slider
-	 */
-
-	$( "#settingspage #font-size").on('change',function (event) {
-			$("#settingspage p:first").css('font-size',$(this).val()+'px' )
-	});	
-	/*
-	 * Save settings function
-	 */
-	
-	$( "#settingspage .back-button").click(function () {
-			window.localStorage.setItem("font-size", $( "#settingspage #font-size").val());
-			applyLocalSettings();
-	});
-	
-
 
 });
